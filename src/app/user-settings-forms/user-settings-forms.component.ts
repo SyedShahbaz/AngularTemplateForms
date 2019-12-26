cdf8561700e1586a0842c12df0fea72ad76f2465
Syed Haider <syed.haider@L-P-A.com>
2019-12-26 21:02:14 +0100

    Data binding in Angular Forms

--- src/app/user-settings-forms/user-settings-forms.component.ts
+++ src/app/user-settings-forms/user-settings-forms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserSettings } from '../data/user-settings';
 
 @Component({
   selector: 'app-user-settings-forms',
@@ -7,6 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserSettingsFormsComponent implements OnInit {
 
+  // We want to populate the form with this data model. we want to update the model and form if any one changes
+  // for that we are using two way data binding.. make sure to use the name attribute to the field inthe form
+  // that crosspondes to the UserSettings field. use <input type='text' name=name [(ngModel)] = usersettings.name.
+  originalUserSettings: UserSettings = {
+    name: 'Syed',
+    emailOffers: true,
+    interfaceStyle: 'dark',
+    subscriptionType: 'Annual',
+    notes: 'Some notes...'
+  };
+
+  // Copy is always good to work on, when user saves or discard the forms input then only update the original object.
+  // else we will get the updated values even if user discards the forms input and decides otherwise.
+  userSettings: UserSettings = {...this.originalUserSettings};
   constructor() { }
 
   ngOnInit() {
